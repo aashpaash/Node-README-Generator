17037ddf953d29b15e7d35ca8f74df8b9c9e741e
aashpaash <115119317+aashpaash@users.noreply.github.com>
2023-05-04 23:50:23 -0700

    fixed syntax in index.js

--- index.js
+++ index.js
@@ -1,65 +1,74 @@
 // TODO: Include packages needed for this application
-const inquire = require("inquirer");
+const inquirer = require("inquirer");
 const fs = require('fs');
-const util = require('util');
-const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-const generateMarkdown = require('./utils/generateMarkdown');
-const { default: inquirer } = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: "input",
-    name: "title",
-    message: "What is the name of this project?",
-
-    type: "input",
-    name: "description",
-    message: "Please decribe your project.",
-    
-    type: "input",
-    name: "objective",
-    message: "What is the project's purpose?",
-
-    type: "input",
-    name: "usage",
-    message: "How can this project be used?",
-
-    type: "input",
-    name: "installation",
-    message: "What dependencies are needed to run this project?",
-    default: "inquirer 8.2.4",
-
-    type: "input",
-    name: "contributors",
-    message: "Who all who contributed to this project",
-
-    type: "input",
-    name: "test",
-    message: "How can project be tested?",
-
-    type: "list",
-    message: "Select a license for this project",
-    choices: [
-         'Unlicense', 'Apache', 'GNU General Public License', 'MIT License'
-    ],
-
-    type: "input",
-    name: "learn",
-    message: "What has this project taught you?",
-
-    type: "input",
-    name: "github",
-    message: "What is your Github username?",
-
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-}];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of this project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please decribe your project.",
+    }, 
+    {
+        type: "input",
+        name: "objective",
+        message: "What is the project's purpose?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How can this project be used?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What dependencies are needed to run this project?",
+        default: "inquirer 8.2.4",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who all who contributed to this project",
+    },   
+    {
+        type: "input",
+        name: "test",
+        message: "How can project be tested?",
+    },
+    {
+        type: "list",
+        message: "Select a license for this project",
+        choices: [
+            'Unlicense', 'Apache', 'GNU General Public License', 'MIT License'
+        ],
+    },
+    {
+        type: "input",
+        name: "learn",
+        message: "What has this project taught you?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+        
+];
 
 // TODO: Create a function to write README file
 function writeREADME(fileName, data) {
-    return fs.writeFile('README', fileName, data, function(err) {
+    return fs.writeFile('README.md', fileName, data, function(err) {
         if (err) {
            return console.log(err);
         } else {
@@ -71,10 +80,9 @@ function writeREADME(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(responses); {
-        console.log("README initialized");
-
-    }
+    .then(function (data) {
+        writeREADME("README.md", generateMarkdown(data));
+    })
 }
 
 // Function call to initialize app
